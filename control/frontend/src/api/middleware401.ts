a14dff7d9093443eb34f010fbd3dd2fd1b6679db
nonJerry <79670160+nonJerry@users.noreply.github.com>
2026-06-13 08:47:39 +0200

    Update csrf on error

--- control/frontend/src/api/middleware401.ts
+++ control/frontend/src/api/middleware401.ts
@@ -8,11 +8,18 @@ import router from '@/router'
  */
 const middleware401 = async (error: AxiosError): Promise<never> => {
     const status = error.response?.status
+    const authStore = useAuthStore()
     if (status === 401 || status === 419) {
-        const auth = useAuthStore()
-        setTimeout(async () => await auth.logout(router), 3000)
-        const rejectionError = new Error('You lost your credentials - will be redirected to login page.')
+        if (authStore.isLoggedIn) {
+            setTimeout(async () => await authStore.logout(router), 3000)
+            const rejectionError = new Error('You lost your credentials - will be redirected to login page.')
+            rejectionError.name = 'PermissionDenied'
+            throw rejectionError;
+        }
+    } else if (status === 403) {
+        const rejectionError = new Error('You are not authorized to access this resource.')
         rejectionError.name = 'PermissionDenied'
+         await authStore.initializeCsrfToken()
         throw rejectionError;
     }
 
