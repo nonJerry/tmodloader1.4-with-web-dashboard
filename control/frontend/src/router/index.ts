f702cf3fd72b8b75c6236771535ec88312eb9008
nonJerry <79670160+nonJerry@users.noreply.github.com>
2026-06-16 22:30:41 +0200

    Add bare bones about
    Avoid arror bc of header

--- control/frontend/src/router/index.ts
+++ control/frontend/src/router/index.ts
@@ -3,6 +3,7 @@ import authMiddleware from './middleware/auth-middleware'
 
 import Dashboard from '@/views/DashboardView.vue'
 import Login from '@/views/auth/LoginView.vue'
+import About from '@/views/AboutView.vue'
 
 
 const router = createRouter({
@@ -10,6 +11,7 @@ const router = createRouter({
   routes: [
     { name: 'dashboard', path: '/', meta: { requiresAuth: true }, component: Dashboard },
     { name: 'login', path: '/login', component: Login, meta: { layout: 'auth' } },
+    { name: 'about', path: '/about', meta: { requiresAuth: true }, component: About },
   ]
 })
 
---
+++ control/frontend/src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="container">
+    <h1>About - Terraria Server Control</h1>
+
+    LOREM IPSUM
+  </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
