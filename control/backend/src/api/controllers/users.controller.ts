c78d7a6197a598bcef0cdd3ac9080819d78fdf3d
nonJerry <79670160+nonJerry@users.noreply.github.com>
2026-06-16 14:32:50 +0200

    Fix unwanted auto logout
    Missed to update the accessToken access in user controller

--- control/backend/src/api/controllers/users.controller.ts
+++ control/backend/src/api/controllers/users.controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
 import { verifyToken } from '../../services/jwt.service.js';
 import users from '../../services/users.service.js';
+import { COOKIE_PREFIX, IS_PRODUCTION } from '../../config/constants.js';
+
+const accessTokenCookie = IS_PRODUCTION ? `${COOKIE_PREFIX}-accessToken` : 'accessToken'
 
 export function getCurrentUser(req: Request, res: Response) {
-    const token = req.cookies?.refreshToken
+    const token = req.cookies?.[accessTokenCookie]
     const username = getAuthenticatedUsername(token)
 
     if (!username) {
